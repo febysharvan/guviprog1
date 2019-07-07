191d4e5d7ccbe9fcef050d1c996159c10618f43b
febysharvan <50619709+febysharvan@users.noreply.github.com>
2019-07-07 09:38:08 +0530

    Create roman.py

---
+++ roman.py
@@ -0,0 +1,5 @@
+a=["","I","II","III","IV","V","VI","VII","VIII","IX","X","XI","XII","XIII","XIV","XV","XVI","XVII","XVIII","XIX","XX"]
+rom=input()
+if rom in a:
+   b=a.index(rom)
+   print(int(b))
